6979a448642dc9bac69a2fc6cdb723417d0eeac2
Bryan Byrne <brybyrne@cisco.com>
2018-04-03 12:35:32 -0400

    Updating shell scripts

--- shell/code_prompt.sh
+++ shell/code_prompt.sh
@@ -4,4 +4,22 @@ parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-export PS1="\W:\033[32m\]\$(parse_git_branch)\[\033[00m\]$ "
+parse_git_dirty() {
+    st=$(git status 2>/dev/null | tail -n 1)
+    if [[ $st == "" ]]; then
+        echo ''
+    elif [[ $st == "nothing to commit (working directory clean)" ]]; then
+        echo ''
+    elif [[ $st == 'nothing added to commit but untracked files present (use "git add" to track)' ]]; then
+        echo '?'
+    else
+        echo '*'
+    fi
+}
+
+# coloring the terminal comman line
+SB_GREEN="\[\033[32m\]"
+SB_BLUE="\[\033[34m\]"
+SB_RED="\[\033[31m\]"
+SB_NOCOLOR="\[\033[0m\]"
+export PS1="\W$SB_GREEN\$(parse_git_branch)$SB_RED\$(parse_git_dirty)$SB_NOCOLOR \$ "
