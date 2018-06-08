f9efc47ae635c49642db80842a5bcd4b238f4253
Bryan Byrne <brybyrne@cisco.com>
2018-06-08 09:58:43 -0400

    Updating Script with Additional Directories.

--- shell/clus18_bysh.sh
+++ shell/clus18_bysh.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-cd ~/workspace/ciscolive2018-brybyrne
+# This is a script to update CLUS DevNet scripts on the Workstation Laptops.
+# There is a bit.ly shortened URL that will redirect here bit.ly/byrneshoe
+# The script can be copied to the workshop with 'curl -L -o filename.sh bit.ly/byrneshoe'
 
+echo
+echo Running Git Pull for ciscolive2018-byrne
+cd ~/workspace/ciscolive2018-brybyrne
 git pull
 
-cd ~/workspace
+echo
+echo
 
+echo Creating ~/workspace/rshoemak and cloning CL-Setup and DevNet2556 repos.
+cd ~/workspace
+mkdir rshoemak && cd rshoemak
 git clone https://www.github.com/rshoemak/DevNet2556/
+git clone https://www.github.com/rshoemak/CL-Setup
